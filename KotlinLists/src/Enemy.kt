48d803a3ec7a0ee076e46eb8ea9120b649a2ab7c
Pierre Goldstein <pierre.goldstein@gmail.com>
2019-10-01 16:41:04 -0700

    Inheritance continued part 2

--- KotlinLists/src/Enemy.kt
+++ KotlinLists/src/Enemy.kt
@@ -1,5 +1,5 @@
 open class Enemy(val name: String, var hitPoints: Int, var lives: Int){
-    fun takeDamage(damage: Int){
+    open fun takeDamage(damage: Int){
         val remainingHitPoint = hitPoints - damage
         if(remainingHitPoint > 0){
             hitPoints = remainingHitPoint
--- KotlinLists/src/Main.kt
+++ KotlinLists/src/Main.kt
@@ -2,48 +2,15 @@ import jdk.nashorn.internal.runtime.JSType.toDouble
 import kotlin.math.pow
 
 fun main(Args: Array<String>){
-    var pierre: Player = Player("Pierre", 3, 1, 0)
-    pierre.show()
 
-    val redPotion = Loot("Red Potion", LootType.POTION, 7.50)
-    pierre.getLoot(redPotion)
-    val chestArmor = Loot("+3 Chest Armor", LootType.ARMOR, 80.00)
-    pierre.getLoot(chestArmor)
-
-    pierre.weapon.name = "LongSword"
-    pierre.weapon.damage = 100
-    val ringOfProtection = Loot("Ring of Protection +2", LootType.RING, 40.25)
-    val invisibilityPotion = Loot("Invisibilty Potion", LootType.POTION, 35.95)
-    pierre.getLoot(ringOfProtection)
-    pierre.getLoot(invisibilityPotion)
-    pierre.showInventory()
-    if(pierre.dropLoot(redPotion)){
-        pierre.showInventory()
-    }else{
-        println("you don't have a ${redPotion.name}")
-    }
-    val bluePotion = Loot("Blue Potion", LootType.POTION, 6.00)
-    if(pierre.dropLoot((bluePotion))){
-        pierre.showInventory()
-    }else{
-        println("you don't have a ${bluePotion.name}")
-    }
-    println(pierre)
-    if (pierre.dropLoot("Invisibilty Potion")){
-        pierre.showInventory()
-    }else{
-        println("you don't have a \"Invisibilty Potion\"")
-    }
-    pierre.showInventory()
-    val enemy = Enemy("My Ex", 5, 9)
-    enemy.takeDamage(4)
-    enemy.toString()
-    enemy.takeDamage(30)
-    enemy.toString()
-    enemy.toString()
-
-    val troll = Troll("gargamel")
+    val troll = Troll("Gargamel")
     println(troll)
     troll.takeDamage(30)
     println(troll)
+
+    println("=======================")
+
+    val vlad = Vampire("Vlad")
+    vlad.takeDamage(9)
+    println(vlad)
 }
